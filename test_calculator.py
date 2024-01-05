faadca53f0cd433de76d201d1ee97a526e901240
JUAN PEDRO MENDOZA RAMIREZ <jpmendoza@universidadean.edu.co>
2024-01-05 15:27:34 -0500

    modificacion de test_calculator

--- test_calculator.py
+++ test_calculator.py
@@ -5,6 +5,8 @@ from calculator import square
 def test_square():
     assert square(2)==4
     assert square(3)==9
-    assert square(4)==16
+def test_square_negative():   
+    assert square(-4)==16
     assert square(-5)==25
+def test_square_zero():
     assert square(0)==0
