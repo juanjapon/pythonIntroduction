724062ae526dca4a8dddd69fff314deace800beb
JUAN PEDRO MENDOZA RAMIREZ <jpmendoza@universidadean.edu.co>
2024-01-22 12:54:18 -0500

    unpacking para numero ideterminado de argumentos en una funcion

--- unpack.py
+++ unpack.py
@@ -1,6 +1,4 @@
-def total(galleons,sickles,knuts):
-    return(galleons*17+sickles)*29+knuts
+def f(*args,**kwargs):
+    print("Positional:",*kwargs.keys())
 
-coins=[100,50,25]
-
-print(total(*coins),"Knuts")
+f(galleons=100,sickles=50,knuts=25)
