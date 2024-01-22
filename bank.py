61e82050fb8ac3b44d00ea45b3eb5e06f2709551
JUAN PEDRO MENDOZA RAMIREZ <jpmendoza@universidadean.edu.co>
2024-01-22 10:23:42 -0500

    global variable

---
+++ bank.py
@@ -0,0 +1,19 @@
+class Account:
+    def __init__(self):
+        self._balance=0
+    @property
+    def balance(self):
+        return self._balance
+    def deposit(self,n):
+        self._balance+=n
+    def withdraw(self,n):
+        self._balance-=n
+
+def main():
+    account=Account()
+    print("Balance:",account.balance)
+    account.deposit(100)
+    account.withdraw(20)
+    print("Balance:",account.balance)
+
+main()
